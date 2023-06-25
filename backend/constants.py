c153fe308e72d8c4333a16224078f4787caada11
BenitzCoding <beni@senarc.net>
2023-06-25 17:49:03 +0800

    Fixed Constant iteration.

--- backend/constants.py
+++ backend/constants.py
@@ -1,6 +1,6 @@
 import json
 
 class Constants:
-    def __init__(self) -> None:
-        with open("config.json", "r") as file:
-            for key, value in json.load(file): setattr(self, key, value)
+	def __init__(self) -> None:
+		with open("config.json", "r") as file:
+			for key, value in json.load(file).items(): setattr(self, key.upper(), value)
